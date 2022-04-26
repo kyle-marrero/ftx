e6cd8508e67bb85d565fb5412763a58e6e6c11d8
Kyle Marrero <kylemarrero@Kyles-MacBook-Pro.local>
2022-04-25 20:25:17 -0500

    starting

---
+++ cpp/bin/src/rest/CMakeFiles/rest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rest.dir/client.cc.o"
+  "CMakeFiles/rest.dir/client.cc.o.d"
+  "librest.a"
+  "librest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cpp/bin/src/util/CMakeFiles/util.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/kylemarrero/Projects/ftx_1/ftx/cpp/src/util/HTTP.cc" "src/util/CMakeFiles/util.dir/HTTP.cc.o" "gcc" "src/util/CMakeFiles/util.dir/HTTP.cc.o.d"
+  "/Users/kylemarrero/Projects/ftx_1/ftx/cpp/src/util/WS.cc" "src/util/CMakeFiles/util.dir/WS.cc.o" "gcc" "src/util/CMakeFiles/util.dir/WS.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
